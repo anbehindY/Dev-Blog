5860e8142f5ea02be0266074cffb753c61862479
anbehindY <yanminthwin@gmail.com>
2023-07-21 19:01:41 +0630

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: "author_id", dependent: :destroy
-  has_many :comments, foreign_key: "author_id", dependent: :destroy
-  has_many :likes, foreign_key: "author_id", dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   def recent_posts
     posts.last(3).reverse
