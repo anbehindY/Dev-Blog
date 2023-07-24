0ba45662e84e15057eea29244975c14aedcce60e
anbehindY <yanminthwin@gmail.com>
2023-07-24 19:57:01 +0630

    Add controllers and routes for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find_by_id(params[:id])
+    @posts = @user.posts
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    @post = @user.posts.find_by_id(params[:post_id])
+  end
+end 
