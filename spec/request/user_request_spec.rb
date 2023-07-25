fd1bf20028ff3fc69c3afe9b830ca15db3d28e9a
anbehindY <yanminthwin@gmail.com>
2023-07-25 20:30:53 +0630

    Refactor tests for user controller

--- spec/request/user_request_spec.rb
+++ spec/request/user_request_spec.rb
@@ -1,19 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :request do
+  let(:user) { User.create!(name: 'anbehindY', photo: 'person.jpg', bio: 'Full-stack web developer') }
+
   context 'GET /users' do
-    it 'should return http success' do
+    it 'should renders a successful response' do
       get '/users'
-      expect(response.status).to eq(200)
       expect(response).to be_successful
+    end
+
+    it 'should render the correct template' do
+      get '/users'
+      expect(response.body).to render_template(:index)
+    end
+
+    it 'should return the correct placeholder text' do
+      get '/users'
+      expect(response.body).to include('List of all users')
     end
   end
 
   context 'GET /users/:id' do
     it 'should renders a successful response' do
-      user = User.create!(name: 'anbehindY', photo: 'person.jpg', bio: 'Full-stack web developer')
       get user_url(user)
       expect(response).to be_successful
+    end
+
+    it 'should render the correct template' do
+      get user_url(user)
+      expect(response.body).to render_template(:show)
+  end
+
+    it 'should return the correct placeholder text' do
+      get user_url(user)
+      expect(response.body).to include('User with the given id')
     end
   end
 end
