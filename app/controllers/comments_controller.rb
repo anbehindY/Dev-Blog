25f9c5930e5682a3f31cc7529514e0f47ca67dcc
anbehindY <yanminthwin@gmail.com>
2023-07-29 00:44:14 +0630

    Fix rubocop errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,17 +3,17 @@ class CommentsController < ApplicationController
     @post = Post.find_by_id(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.author = current_user
-    
+
     if @comment.save
-      flash[:success] = "Comment added!"
+      flash[:success] = 'Comment added!'
       redirect_to user_post_path(current_user, @comment.post)
     else
       flash[:errors] = @comment.errors.full_messages
     end
   end
-  
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:text)
   end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,9 +2,9 @@ class LikesController < ApplicationController
   def create
     @post = Post.find_by_id(params[:post_id])
     @like = @post.likes.new(author: current_user)
-    
+
     if @like.save
-      flash[:success] = "Like added!"
+      flash[:success] = 'Like added!'
       redirect_to user_post_path(current_user, @like.post)
     else
       flash[:errors] = @like.errors.full_messages
