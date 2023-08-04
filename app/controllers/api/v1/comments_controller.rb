5d82c6239134c06d0e3039d7ed0147aed8a90730
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-04 16:02:13 -0400

    Add comments controller api

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::CommentsController < ApplicationController
+    def index
+      @post = Post.find_by_id(params[:post_id])
+      @comments = @post.comments
+      render json: @comments
+    end
+  
+    def create
+      @post = Post.find_by_id(params[:post_id])
+      @comment = @post.comments.new(comment_params)
+      @comment.author = current_user
+  
+      if @comment.save
+        render json: @comment
+      else
+        render json: { errors: @comment.errors.full_messages }
+      end
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
